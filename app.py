ee1fc32bafa5b2e660e683d70bfdd61baba64fb8
boujmaaarab98-debug <boujmaaarab98@gmail.com>
2026-04-20 00:05:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,12 @@
 import streamlit as st
 from data.google_sheets import load_all_data
 
-st.title("Test Google Sheets")
+st.title("Test Colonnes")
 
 data = load_all_data()
 
-st.subheader("Param")
-st.write(data["param"])
+st.subheader("Colonnes Param")
+st.write(list(data["param"].columns))
 
-st.subheader("MRP")
-st.write(data["mrp"])
-
-st.subheader("Fournisseurs")
-st.write(data["fournisseurs"])
-
-st.subheader("Conso")
-st.write(data["conso"])
-
-import pandas as pd
-
-def calculate_plan(param, conso):
-    # merge data
-    df = param.merge(conso, on="code_mp")
-
-    # consommation journalière
-    df["conso_j"] = df["conso_mensuelle"] / 30
-
-    # couverture
-    df["couverture_j"] = df["stock_actuel"] / df["conso_j"]
-
-    # besoin
-    df["besoin"] = df["lead_time_j"] - df["couverture_j"]
-
-    # decision
-    df["a_commander"] = df["besoin"] > 0
-
-    # quantité
-    df["qte_commande"] = df.apply(
-        lambda x: x["moq_kg"] if x["a_commander"] else 0,
-        axis=1
-    )
-
-    return df
-
-data = load_all_data()
-
-df = calculate_plan(data["param"], data["conso"])
-
-st.subheader("Plan Approvisionnement")
-st.write(df)
+st.subheader("Colonnes Conso")
+st.write(list(data["conso"].columns))
