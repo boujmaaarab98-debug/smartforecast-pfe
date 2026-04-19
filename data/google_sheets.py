1eb4e4e29f47efa40359452a9d1da1c4cd92fbd1
boujmaaarab98-debug <boujmaaarab98@gmail.com>
2026-04-19 23:54:16 +0200

    Create google_sheets.py

---
+++ data/google_sheets.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+SHEET_ID = "حط هنا ID ديال Google Sheet ديالك"
+
+def build_url(sheet_name):
+    return f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+def load_sheet(sheet_name):
+    url = build_url(sheet_name)
+    return pd.read_csv(url)
+
+def load_all_data():
+    return {
+        "param": load_sheet("Param"),
+        "mrp": load_sheet("MRP"),
+        "fournisseurs": load_sheet("Fournisseurs"),
+        "conso": load_sheet("Conso"),
+    }
