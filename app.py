0bc22f30a4dde0b826825af138eb441a835b87f2
boujmaaarab98-debug <boujmaaarab98@gmail.com>
2026-04-19 23:55:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from data.google_sheets import load_all_data
 
+st.title("Test Google Sheets")
+
+data = load_all_data()
+
+st.subheader("Param")
+st.write(data["param"])
+
+st.subheader("MRP")
+st.write(data["mrp"])
+
+st.subheader("Fournisseurs")
+st.write(data["fournisseurs"])
+
+st.subheader("Conso")
+st.write(data["conso"])
